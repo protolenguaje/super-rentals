c1f7c0f4ed7ae8310d084dd1812265f66ba07103
protolenguaje <ivan.glez.aguilar@gmail.com>
2016-07-23 19:38:01 -0500

    fix of rental component test

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -24,6 +24,6 @@ test('should toggle wide class on click', function(assert) {
   assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
   this.$('.image').click();
   assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
-  this.$('image').click();
+  this.$('.image').click();
   assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
